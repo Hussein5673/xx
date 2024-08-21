5ca1b58cf5c80adb09d374a325752b3563723cfe
omran <omrankh2000@hotmail.com>
2024-08-21 10:57:35 +0300

    Pushing the database

---
+++ 127_0_0_1.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2024 at 09:56 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_base`
+--
+CREATE DATABASE IF NOT EXISTS `user_base` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `user_base`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_title`
+--
+
+CREATE TABLE `game_title` (
+  `ID` int(11) NOT NULL,
+  `Game_Title` text NOT NULL,
+  `Release_Year` int(11) NOT NULL,
+  `Publisher` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `game_title`
+--
+
+INSERT INTO `game_title` (`ID`, `Game_Title`, `Release_Year`, `Publisher`) VALUES
+(2, 'GTA V', 2013, 'Rockstar Games'),
+(3, 'Elden Ring', 2022, 'FromSoftware');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL,
+  `Name` text NOT NULL,
+  `Email` text NOT NULL,
+  `UserName` text NOT NULL,
+  `Password` text NOT NULL,
+  `DateOfBirth` text NOT NULL,
+  `Friends` text NOT NULL,
+  `reg_date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Id`, `Name`, `Email`, `UserName`, `Password`, `DateOfBirth`, `Friends`, `reg_date`) VALUES
+(1, 'Waleed', 'Waleed123@gmail.com', 'myuser', '123', '04/07/2000', 'Omran,Waleed', '2024-08-20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `game_title`
+--
+ALTER TABLE `game_title`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `game_title`
+--
+ALTER TABLE `game_title`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
