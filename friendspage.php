ec84f2d2c482f15edca6fe01c0ddbb275ab9ed88
waleedkhaledalghool <119655482+waleedkhaledalghool@users.noreply.github.com>
2024-08-28 05:03:25 +0300

    fixed the css of the friends page

--- friendspage.php
+++ friendspage.php
@@ -40,8 +40,142 @@ sqlsrv_close($conn);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap&family=Abel&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="styles.css">
-  <!-- Your existing styles -->
+  <style>
+    body {
+        background-color: #0a1a29;
+        color: white;
+        margin: 0;
+        font-family: 'Abel', sans-serif;
+    }
+
+    .FriendsPageMichel {
+        padding: 20px;
+    }
+
+    .GameVault {
+        font-size: 3vw;
+        font-family: 'Righteous', cursive;
+    }
+
+    .FriendsPage {
+        font-size: 2vw;
+        font-family: 'Abel', sans-serif;
+        text-align: center;
+        margin-top: 10px;
+    }
+
+    .UserName {
+        font-size: 2.5vw;
+        font-family: 'Righteous', cursive;
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    .OnlineStatus {
+        font-size: 1.5vw;
+        color: lightgreen;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .OnlineStatus .dot {
+        height: 12px;
+        width: 12px;
+        background-color: lightgreen;
+        border-radius: 50%;
+        display: inline-block;
+        margin-right: 10px;
+    }
+
+    .Frame379 {
+        width: 35%;
+        height: calc(100% - 310px);
+        background: rgba(21, 69, 94, 0.44);
+        padding: 20px;
+        box-sizing: border-box;
+        margin-top: 20px;
+        overflow-y: auto;
+        border-radius: 10px;
+    }
+
+    .YourFriends {
+        font-size: 2.5vw;
+        font-family: 'Abel', sans-serif;
+        margin-bottom: 15px;
+    }
+
+    .Frame379 ul {
+        padding-left: 20px;
+        list-style-type: none;
+    }
+
+    .Frame379 ul li {
+        margin-bottom: 10px;
+    }
+
+    .Frame379 ul li button {
+        background: none;
+        border: none;
+        color: white;
+        font-size: 2vw;
+        font-family: 'Abel', sans-serif;
+        cursor: pointer;
+    }
+
+    .SearchResults {
+        width: 60%;
+        height: calc(100% - 310px);
+        background: rgba(255, 255, 255, 0.9);
+        padding: 20px;
+        box-sizing: border-box;
+        margin-left: 37%;
+        margin-top: -280px;
+        border-radius: 10px;
+        overflow-y: auto;
+    }
+
+    .SearchResults div {
+        color: black;
+        font-family: 'Abel', sans-serif;
+        font-size: 2vw;
+        margin-bottom: 20px;
+    }
+
+    .Searchbox {
+        margin-top: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+    .SearchInput {
+        padding: 10px;
+        width: 70%;
+        border-radius: 5px;
+        border: none;
+        font-size: 1.2vw;
+    }
+
+    .Search button {
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+        background-color: #15455e;
+        color: white;
+        font-size: 1.2vw;
+        cursor: pointer;
+    }
+
+    .Search button:hover {
+        background-color: #0a1a29;
+    }
+  </style>
 </head>
 <body>
 <div class="FriendsPageMichel">
